e3ef464ab174b48509c4c5a0891622b8b020592e
yangeunchan <qud1251@aswemake.com>
2023-06-24 22:41:40 +0900

    base setting

---
+++ src/main/kotlin/com/example/elasticsearch/ElasticSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.example.elasticsearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElasticSearchApplication
+
+fun main(args: Array<String>) {
+    runApplication<ElasticSearchApplication>(*args)
+}
